3a5b462fae07c8029f090e80493ecd1c4f7641aa
Juraj Martinka <jumarko@gmail.com>
2019-02-11 12:38:53 +0100

    Ep. 7: twitter - fix tweets printing and simple dedup by maintaining a set of previous tweets.
    The "set approach" doesn't scale because we dont' evict old items => OOM.

--- twitter/src/twitter/core.clj
+++ twitter/src/twitter/core.clj
@@ -5,34 +5,50 @@
    [twitter.api :as api]))
 
 (def twitter-creds (edn/read-string (slurp ".creds.edn")))
+(def sleep-time 15000)
 
 (defn- authenticate []
   (api/authenticate twitter-creds))
 
 (defn- search [auth-handle]
-  (api/search auth-handle "#clojure"))
+  (try 
+    (api/search auth-handle "#clojure")
+    (catch Exception e
+      ;; TODO: it might be too late to catch data here
+      ;; since we would need reponse boy if non-ok HTTP status is thrown
+      ;; => if we don't do that in `twitter.api` ns then http client details will leak
+      (pst e))))
 
-(defn format-tweet [tweet]
-  (format "%s tweeted: '%s'\n"
-          (:user/name tweet)
-          (-> tweet :tweet/text (clojure.string/replace "\n" " "))))
+(defn format-tweets [tweets]
+  (let [format-tweet (fn format-tweet [tweet]
+                       (format " * %s tweeted: '%s'"
+                               (:user/name tweet)
+                               (some-> tweet :tweet/text (clojure.string/replace "\n" " "))))]
+    (if (empty? tweets)
+      ["NO NEW TWEETS!"]
+      (mapv format-tweet tweets))))
+
+;; TODO: use proper cache evicting old entries to prevent out of memory
+;; Check https://github.com/clojure/core.cache/wiki/Using
+(defn remove-already-seen-tweets [seen tweets]
+  (let [new-tweets (remove (fn [{:tweet/keys [id]}]
+                             (contains? seen id))
+                           tweets)]
+    ;; (prn "DEBUG:: " tweets)
+    ;; (prn "DEBUG:: " new-tweets)
+    [new-tweets (apply conj seen (map :tweet/id new-tweets))]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [auth-handle (authenticate)]
-    (loop []
-      (try
-        (let [tweets (search auth-handle)]
-          ;; TODO: dedup tweets
-          (run! println (mapv format-tweet tweets)))
-        (catch Exception e
-          ;; TODO: it might be too late to catch data here
-          ;; since we would need reponse boy if non-ok HTTP status is thrown
-          ;; => if we don't do that in `twitter.api` ns then http client details will leak
-          (pst e)))
-      (Thread/sleep 15000)
-      (recur))))
+    (loop [seen #{}] ;; `seen` is set of tweet ids
+      (let [tweets (or (search auth-handle)
+                       [])
+            [new-tweets updated-seen] (remove-already-seen-tweets seen tweets)]
+        (run! println (format-tweets new-tweets))
+        (Thread/sleep sleep-time)
+        (recur updated-seen)))))
 
 (comment
   (def my-creds (api/authenticate twitter-creds))
--- twitter/test/twitter/core_test.clj
+++ twitter/test/twitter/core_test.clj
@@ -2,12 +2,29 @@
   (:require [clojure.test :refer [deftest is testing]]
             [twitter.core :as c]))
 
-(deftest format-tweet
-  (testing "Tweet is properly formatted"
-    (is (= "Juraj tweeted: 'Check out Clojure - it's cool!'\n"
-         (c/format-tweet {:user/name "Juraj"
-                            :tweet/text "Check out Clojure - it's cool!"}))))
+(deftest format-tweets
+  (testing "no tweets"
+    (is (= ["NO NEW TWEETS!" ]
+           (c/format-tweets []))))
+  (testing "Tweets are properly formatted"
+    (is (= [" * Juraj tweeted: 'Check out Clojure - it's cool!'"
+            " * Joseph tweeted: 'React is cool too!'"]
+           (c/format-tweets [{:user/name "Juraj" :tweet/text "Check out Clojure - it's cool!"}
+                             {:user/name "Joseph" :tweet/text "React is cool too!"}
+                             ]))))
   (testing "Newline is replaced with space"
-    (is (= "Juraj tweeted: 'First tweet line. Second tweet line'\n"
-           (c/format-tweet {:user/name "Juraj"
-                            :tweet/text "First tweet line.\nSecond tweet line"})))))
+    (is (= [" * Juraj tweeted: 'First tweet line. Second tweet line'"]
+           (c/format-tweets [{:user/name "Juraj"
+                              :tweet/text "First tweet line.\nSecond tweet line"}])))))
+
+(deftest remove-already-seen-tweets
+  (let [tweets [{:tweet/id "123" :tweet/text "OLDEST tweet" :user/name "Juraj"}
+                {:tweet/id "456" :tweet/text "OLD tweet" :user/name "Joseph"}
+                {:tweet/id "789" :tweet/text "NEW tweet" :user/name "Adam"}]]
+    (testing "All tweets are returned new when the cache is empty"
+      (is (= [tweets #{"123" "456" "789"}]
+             (c/remove-already-seen-tweets #{} tweets))))
+    (testing "Only new tweets are returned"
+      (is (= [(drop 2 tweets) #{"123" "456" "789"}]
+             (c/remove-already-seen-tweets #{"123" "456"} tweets)))))
+  )
