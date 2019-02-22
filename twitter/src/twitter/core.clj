3d6975a6ca776cfdaf243a34f2e02bda0803de3d
Juraj Martinka <jumarko@gmail.com>
2019-02-22 09:48:43 +0100

    Episode 9: twitter app - Extract tweet processing logic from core to separate processor.clj

--- twitter/src/twitter/core.clj
+++ twitter/src/twitter/core.clj
@@ -1,55 +1,28 @@
 (ns twitter.core
   (:require
    [clojure.edn :as edn]
-   [clojure.repl :refer [pst]]
    [twitter.api :as api]
+   [twitter.processor :as processor]
    [twitter.server :as server]))
 
-(def twitter-creds (edn/read-string (slurp ".creds.edn")))
+;; TODO: externalize configuration completely
 (def sleep-time 15000)
+(def twitter-creds (edn/read-string (slurp ".creds.edn")))
 
 (defn- authenticate []
   (api/authenticate twitter-creds))
 
-(defn- search [auth-state]
-  (try 
-    (api/search auth-state "#clojure")
-    (catch Exception e
-      ;; TODO: it might be too late to catch data here
-      ;; since we would need reponse boy if non-ok HTTP status is thrown
-      ;; => if we don't do that in `twitter.api` ns then http client details will leak
-      (pst e))))
-
-(defn format-tweets [tweets]
-  (let [format-tweet (fn format-tweet [tweet]
-                       (format " * %s tweeted: '%s'"
-                               (:user/name tweet)
-                               (some-> tweet :tweet/text (clojure.string/replace "\n" " "))))]
-    (if (empty? tweets)
-      []
-      (mapv format-tweet tweets))))
-
-;; TODO: use proper cache evicting old entries to prevent out of memory
-;; Check https://github.com/clojure/core.cache/wiki/Using
-(defn remove-already-seen-tweets [seen tweets]
-  (let [new-tweets (remove (fn [{:tweet/keys [id]}]
-                             (contains? seen id))
-                           tweets)]
-    ;; (prn "DEBUG:: " tweets)
-    ;; (prn "DEBUG:: " new-tweets)
-    [new-tweets (apply conj seen (map :tweet/id new-tweets))]))
+(defn twitter-loop []
+  (loop [auth-state (authenticate)
+         seen #{}] ;; `seen` is set of tweet ids
+    (let [[updated-auth-state updated-seen] (processor/process-tweets auth-state seen sleep-time)]
+      (recur updated-auth-state updated-seen))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (server/start-server)
-  (loop [auth-state (authenticate)
-         seen #{}] ;; `seen` is set of tweet ids
-    (let [[updated-auth-state tweets] (or (search auth-state) [auth-state []])
-         [new-tweets updated-seen] (remove-already-seen-tweets seen tweets)]
-    (run! println (format-tweets new-tweets))
-    (Thread/sleep sleep-time)
-    (recur updated-auth-state updated-seen))))
+  (twitter-loop))
 
 (comment
   (def my-auth-state (api/authenticate twitter-creds))
@@ -58,6 +31,6 @@
 
   (def main-future (future (-main)))
 
-  (future-cancel main-future)
+  (future-cancel
 
-  )
+   main-future))
--- twitter/test/twitter/core_test.clj
+++ twitter/test/twitter/core_test.clj
@@ -2,29 +2,3 @@
   (:require [clojure.test :refer [deftest is testing]]
             [twitter.core :as c]))
 
-(deftest format-tweets
-  (testing "no tweets"
-    (is (= []
-           (c/format-tweets []))))
-  (testing "Tweets are properly formatted"
-    (is (= [" * Juraj tweeted: 'Check out Clojure - it's cool!'"
-            " * Joseph tweeted: 'React is cool too!'"]
-           (c/format-tweets [{:user/name "Juraj" :tweet/text "Check out Clojure - it's cool!"}
-                             {:user/name "Joseph" :tweet/text "React is cool too!"}
-                             ]))))
-  (testing "Newline is replaced with space"
-    (is (= [" * Juraj tweeted: 'First tweet line. Second tweet line'"]
-           (c/format-tweets [{:user/name "Juraj"
-                              :tweet/text "First tweet line.\nSecond tweet line"}])))))
-
-(deftest remove-already-seen-tweets
-  (let [tweets [{:tweet/id "123" :tweet/text "OLDEST tweet" :user/name "Juraj"}
-                {:tweet/id "456" :tweet/text "OLD tweet" :user/name "Joseph"}
-                {:tweet/id "789" :tweet/text "NEW tweet" :user/name "Adam"}]]
-    (testing "All tweets are returned new when the cache is empty"
-      (is (= [tweets #{"123" "456" "789"}]
-             (c/remove-already-seen-tweets #{} tweets))))
-    (testing "Only new tweets are returned"
-      (is (= [(drop 2 tweets) #{"123" "456" "789"}]
-             (c/remove-already-seen-tweets #{"123" "456"} tweets)))))
-  )
