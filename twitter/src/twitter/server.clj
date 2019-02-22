11b518d86882865f5745ec26650b527b53f5b940
Juraj Martinka <jumarko@gmail.com>
2019-02-22 09:17:25 +0100

    Episode 9: twitter app - track server state internally and don't expose it to the client.

--- twitter/src/twitter/server.clj
+++ twitter/src/twitter/server.clj
@@ -1,6 +1,8 @@
 (ns twitter.server
-  "HTTP server running on port 8081 and listening for search queries on `/search` endpoint (POST)
-  and updating the main thread to take the new query into account."
+  "Stateful HTTP server running on port 8081 and listening for search queries on `/search` endpoint (POST)
+  and updating the main thread to take the new query into account.
+  Can be started stopped via given methods.
+  The HTTP server instance is tracked internally and not exposed to the client."
   (:require [aleph.http :as http]
             [compojure.core :refer [defroutes GET POST]]))
 
@@ -9,25 +11,27 @@
 
 (defonce ^:private server (atom nil))
 
-(defn stop-server [server]
-  (when server
+(defn stop-server []
+  (when @server
     (println "Stopping the server...")
-    (.close server)))
+    (.close @server)))
 
 (defn start-server []
-  (stop-server @server)
+  (stop-server)
   (println "Starting the server...")
   (reset! server
           (http/start-server app
+                             ;; TODO: port should be configurable
                              {:port 8081}))
-  (println "Server started."))
+  (println "Server started.")
+  )
 
 
 
 (comment
 
-  (def my-server (start-server))
+  (start-server)
 
-
+  (stop-server)
 
   )
