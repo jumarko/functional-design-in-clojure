5e7b7819a40033cf641d19381151551ed56df6f3
Juraj Martinka <jumarko@gmail.com>
2019-08-30 11:15:12 +0200

    Update to clojure 1.10.1

--- twitter/project.clj
+++ twitter/project.clj
@@ -3,7 +3,7 @@
   :url "https://clojuredesign.club/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [aleph "0.4.6"]
                  [cheshire "5.8.1"]
                  [org.clojure/core.async "0.4.490"]
